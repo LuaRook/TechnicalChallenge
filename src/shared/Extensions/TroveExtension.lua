47e00bc9c71d7e111581bac6a6f304002406f819
LuaRook <rooktube1@gmail.com>
2024-01-27 16:44:35 -0500

    chore: add documentation to TroveExtension.lua

--- src/shared/Extensions/TroveExtension.lua
+++ src/shared/Extensions/TroveExtension.lua
@@ -3,11 +3,14 @@ local Trove = require(ReplicatedStorage.Packages.Trove)
 
 local TroveExtension = {}
 
+-- Create trove when component is constructing.
 function TroveExtension.Constructing(component)
 	component._trove = Trove.new()
 end
 
+-- Clean trove when component is stopping.
 function TroveExtension.Stopping(component)
+	-- Destroy is shorthand for Clean, so call Clean directly.
 	component._trove:Clean()
 end
 
