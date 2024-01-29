4f2e10bf6b98efc00ff61fee513f6846d4944004
LuaRook <rooktube1@gmail.com>
2024-01-28 20:40:19 -0500

    chore: configure basic level

---
+++ src/server/Services/GameService/Levels/Basic.lua
@@ -0,0 +1,19 @@
+--[[
+	File: Basic.lua
+	Author(s): Rook Fait
+	Created: 01/28/2024 @ 20:21:22
+	Version: 0.0.1
+
+	Description:
+		No description provided.
+
+--]]
+
+--[ Export ]--
+return {
+	Enemies = {
+		{
+			Type = "UFO",
+		},
+	},
+}
