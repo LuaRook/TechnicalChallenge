d88a56ed7dc5dc63b1c030d797fa16485c29fe10
LuaRook <rooktube1@gmail.com>
2024-01-28 20:59:03 -0500

    refactor: refactor friendly spawning

--- src/server/Components/SpawnPad.lua
+++ src/server/Components/SpawnPad.lua
@@ -9,16 +9,19 @@
 --]]
 
 --[ Roblox Services ]--
-local CollectionService = game:GetService("CollectionService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --[ Dependencies ]--
 local Component = require(ReplicatedStorage.Packages.Component)
 local Timer = require(ReplicatedStorage.Packages.Timer)
-local RigFriendly = require(script.Parent.Parent.Modules.RigFriendly)
+local Knit = require(ReplicatedStorage.Packages.Knit)
 
 --[ Extensions ]--
 local TroveExtension = require(ReplicatedStorage.Shared.Extensions.TroveExtension)
+
+--[ Services ]--
+-- This is safe to do as our boostrapper loads components after knit has started
+local GameService = Knit.GetService("GameService")
 
 --[ Root ]--
 local SpawnPad = Component.new({
@@ -27,104 +30,13 @@ local SpawnPad = Component.new({
 	Extensions = { TroveExtension },
 })
 
---[ Object References ]--
-local Assets: Folder = ReplicatedStorage.Assets
-local FriendlyAssets: Folder = Assets.Friendlies
-local FriendliesFolder: Folder = workspace.Friendlies
-
---[ Constants ]--
-local SPAWNED_TAG: string = "SpawnedFriendly"
-local DEFAULT_TYPE: string = "Cow"
-local ROTATION_AMOUNT: number = 75
-local MAX_FRIENDLIES: number = 5
-
---[ Local Functions ]--
-
---[[
-	Returns random point within the specified part.
-
-	@param part BasePart The part to get the point for.
-	@return Vector3 The random point within the specified part.
-]]
-local function GetRandomPointInPart(part: BasePart): Vector3
-	local halfX: number = part.Size.X / 2
-	local halfZ: number = part.Size.Z / 2
-
-	return part.Position + Vector3.new(math.random(-halfX, halfX), 0, math.random(-halfZ, halfZ))
-end
-
---[[
-	Returns boolean determining if the friendly spawn limit has been reached.
-
-	@return boolean Boolean determining if the friendly spawn limit has been reached.
-]]
-local function IsFriendlyLimitReached(): boolean
-	return #CollectionService:GetTagged(SPAWNED_TAG) >= MAX_FRIENDLIES
-end
-
---[ Public Functions ]--
-
---[[
-	Spawns entity at a random point within the pad.
-]]
-function SpawnPad:Spawn()
-	-- Check if another entity can be spawned
-	if IsFriendlyLimitReached() then
-		return
-	end
-
-	-- Check if friendly template exists
-	if not self.FriendlyAsset then
-		return
-	end
-
-	-- Spawn entity at a random point
-	local friendlyAsset: BasePart = self._spawnTrove:Clone(self.FriendlyAsset)
-	local randomPoint: Vector3 = GetRandomPointInPart(self.Instance)
-	local yOffset: Vector3 = Vector3.yAxis * (friendlyAsset.Size.Y / 2)
-
-	-- Position friendly with random orientation
-	friendlyAsset.CFrame = CFrame.new(randomPoint + yOffset)
-		* CFrame.Angles(0, math.random(-ROTATION_AMOUNT, ROTATION_AMOUNT), 0)
-
-	-- Add spawned tag to friendly asset
-	friendlyAsset.Parent = FriendliesFolder
-	friendlyAsset:AddTag(SPAWNED_TAG)
-
-	-- Rig friendly model so it can be beamed up
-	RigFriendly(friendlyAsset)
-end
-
---[[
-	Spawns in the maximum amount of friendlies at the pad.
-]]
-function SpawnPad:SpawnMaximum()
-	for _ = 1, MAX_FRIENDLIES do
-		-- Break if limit is reached
-		if IsFriendlyLimitReached() then
-			break
-		end
-
-		self:Spawn()
-	end
-end
-
---[[
-	Despawns all spawned friendlies.
-]]
-function SpawnPad:DespawnAll()
-	self._spawnTrove:Clean()
-end
-
 --[ Initializers ]--
 function SpawnPad:Start()
-	self._spawnTrove = self._trove:Extend()
 	self.SpawnInterval = self.Instance:GetAttribute("SpawnInterval")
-	self.FriendlyAsset = FriendlyAssets:FindFirstChild(self.Instance:GetAttribute("FriendlyType") or DEFAULT_TYPE)
 
 	-- Interval-based spawning
 	self._trove:Add(Timer.Simple(self.SpawnInterval, function()
-		self:Spawn()
+		GameService:SpawnFriendly(self.Instance)
 	end))
 end
 
--- src/server/Services/GameService/init.lua
+++ src/server/Services/GameService/init.lua
@@ -5,14 +5,16 @@
 	Version: 0.0.1
 
 	Description:
-		Handles gameplay logic.
+		Handles gameplay logic such as level selection and spawning.
 --]]
 
 --[ Roblox Services ]--
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
 
 --[ Dependencies ]--
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local RigFriendly = require(script.Parent.Parent.Modules.RigFriendly)
 
 --[ Root ]--
 local GameService = Knit.CreateService({
@@ -21,33 +23,75 @@ local GameService = Knit.CreateService({
 })
 
 --[ Exports & Types & Defaults ]--
-
 type EnemyData = {
 	Type: string, -- The enemy type
 	Origin: Vector3?, -- The origin of the enemy
 }
-
 type LevelData = {
 	Enemies: { EnemyData },
+	Friendlies: { string },
 }
 
 --[ Object References ]--
-
 local Assets: Folder = ReplicatedStorage.Assets
 local EnemyAssets: Folder = Assets.Enemies
 local EnemyFolder: Folder = workspace.Enemies
+local FriendlyAssets: Folder = Assets.Friendlies
 local FriendlyFolder: Folder = workspace.Friendlies
-
 local LevelConfigs: Folder = script:WaitForChild("Levels")
 
---[ Shorthands ]--
+--[ Constants ]--
+local SPAWNED_TAG: string = "SpawnedFriendly"
+local ROTATION_AMOUNT: number = 75
+local MAX_FRIENDLIES: number = 5
 
 --[ Local Functions ]--
+
+--[[
+	Returns boolean determining if the friendly spawn limit has been reached.
+
+	@return boolean Boolean determining if the friendly spawn limit has been reached.
+]]
+local function IsFriendlyLimitReached(): boolean
+	return #CollectionService:GetTagged(SPAWNED_TAG) >= MAX_FRIENDLIES
+end
+
+--[[
+	Returns random point within the specified part.
+
+	@param part BasePart The part to get the point for.
+	@return Vector3 The random point within the specified part.
+]]
+local function GetRandomPointInPart(part: BasePart): Vector3
+	local halfX: number = part.Size.X / 2
+	local halfZ: number = part.Size.Z / 2
+
+	return part.Position + Vector3.new(math.random(-halfX, halfX), 0, math.random(-halfZ, halfZ))
+end
+
+function GameService:_getRandomFriendlyType(): string
+	-- Check if current level exists
+	if not self.CurrentLevel then
+		return
+	end
+
+	-- If length of friendlies is zero, return first index.
+	local validFriendlies: { string } = self.CurrentLevel.Friendlies
+	if validFriendlies == 1 then
+		return validFriendlies
+	end
+
+	-- Return random friendly
+	return validFriendlies[math.random(1, #validFriendlies)]
+end
 
 --[ Public Functions ]--
 
 --[[
 	Spawns enemy of the specified type at the specified origin.
+
+	@param enemyType string The type of enemy to spawn.
+	@param origin Vector3 The origin of the spawned enemy.
 ]]
 function GameService:SpawnEnemy(enemyType: string, origin: Vector3?)
 	local enemyTemplate: BasePart? = EnemyAssets:FindFirstChild(enemyType)
@@ -61,9 +105,60 @@ function GameService:SpawnEnemy(enemyType: string, origin: Vector3?)
 end
 
 --[[
-	Starts game 
+	Spawns friendly at a random spawning pad.
+
+	@param spawnBounds BasePart The bounds for the friendly to spawn within.
+]]
+function GameService:SpawnFriendly(spawnBounds: BasePart)
+	-- Check if another entity can be spawned
+	if IsFriendlyLimitReached() or not self:IsGameActive() then
+		return
+	end
+
+	-- Attempt to get friendly type
+	local friendlyType: string = self:_getRandomFriendlyType()
+	local friendlyTemplate: BasePart? = FriendlyAssets:FindFirstChild(friendlyType)
+	if not friendlyTemplate then
+		return
+	end
+
+	-- Spawn entity at a random point
+	local friendlyClone: BasePart = friendlyTemplate:Clone()
+	local randomPoint: Vector3 = GetRandomPointInPart(spawnBounds)
+	local yOffset: Vector3 = Vector3.yAxis * (friendlyClone.Size.Y / 2)
+
+	-- Position friendly with random orientation
+	friendlyClone.CFrame = CFrame.new(randomPoint + yOffset)
+		* CFrame.Angles(0, math.random(-ROTATION_AMOUNT, ROTATION_AMOUNT), 0)
+
+	-- Add spawned tag to friendly asset
+	friendlyClone.Parent = FriendlyFolder
+	friendlyClone:AddTag(SPAWNED_TAG)
+
+	-- Rig friendly model so it can be beamed up
+	RigFriendly(friendlyClone)
+end
+
+--[[
+	Returns boolean that determines if the game is active or not.
+
+	@return boolean Boolean that determines if the game is active or not.
+]]
+function GameService:IsGameActive(): boolean
+	return self.GameActive
+end
+
+--[[
+	Starts game with the specified level.
+
+	@param levelName string The name of the level to load.
 ]]
 function GameService:StartGame(levelName: string)
+	-- Prevent another game from being started during an active game
+	if self:IsGameActive() then
+		return warn("Cannot start game during an active game!")
+	end
+
 	-- Get level module from name
 	local dataModule: ModuleScript? = LevelConfigs:FindFirstChild(levelName)
 	if not dataModule then
@@ -77,12 +172,18 @@ function GameService:StartGame(levelName: string)
 	for _, enemy: EnemyData in levelData.Enemies do
 		self:SpawnEnemy(enemy.Type, enemy.Origin)
 	end
+
+	self.GameActive = true
+	self.CurrentLevel = levelData
 end
 
 --[[
 	Ends game by removing both enemies and spawned friendlies.
 ]]
 function GameService:EndGame()
+	self.GameActive = false
+	self.CurrentLevel = nil
+
 	EnemyFolder:ClearAllChildren()
 	FriendlyFolder:ClearAllChildren()
 end
