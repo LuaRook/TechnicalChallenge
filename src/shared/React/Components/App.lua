864810c86d012a729ad5eecf1a6faaf549832d96
LuaRook <rooktube1@gmail.com>
2024-01-29 22:48:41 -0500

    refactor: refactor app

--- src/shared/React/Components/App.lua
+++ src/shared/React/Components/App.lua
@@ -10,25 +10,45 @@
 
 --[ Roblox Services ]--
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 
 --[ Dependencies ]--
 local React = require(ReplicatedStorage.Packages.React)
+local Knit = require(ReplicatedStorage.Packages.Knit)
 
 --[ React Components ]--
 local Menu = require(script.Parent.Menu)
 local HUD = require(script.Parent.HUD)
+
+--[ Services & Controllers ]--
+local SoundtrackController = Knit.GetController("SoundtrackController")
+
+--[ Object References ]--
+
+local LocalPlayer: Player = Players.LocalPlayer
 
 --[ Return Component ]--
 return function()
 	-- Create state for HUD visibility
 	local showHUD: boolean, setShowHUD: (newVisibility: boolean) -> () = React.useState(false)
 
-	-- Handle visibility
-	React.useEffect(function() end)
+	-- Update logic for HUD visibility
+	React.useEffect(function()
+		-- Update soundtrack based on HUD visibility
+		SoundtrackController:PlayTrack(if showHUD then "Game" else "Menu")
+
+		-- Set attribute for player on menu
+		LocalPlayer:SetAttribute("OnMenu", not showHUD)
+	end, { showHUD })
 
 	-- Return app
 	return React.createElement(React.Fragment, nil, {
-		Menu = not showHUD and React.createElement(Menu), -- Render menu if HUD is disabled
-		HUD = showHUD and React.createElement(HUD), -- Render HUD if enabled
+		-- Render menu if HUD is disabled
+		Menu = not showHUD and React.createElement(Menu, {
+			SetHUDVisible = setShowHUD,
+		}),
+
+		-- Render HUD if enabled
+		HUD = showHUD and React.createElement(HUD),
 	})
 end
