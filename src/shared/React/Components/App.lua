24a044b7682e26eba1200bc1a743acfc5d910e5c
LuaRook <rooktube1@gmail.com>
2024-01-29 21:54:46 -0500

    chore: add visibility to HUD

--- src/shared/React/Components/App.lua
+++ src/shared/React/Components/App.lua
@@ -13,11 +13,22 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 --[ Dependencies ]--
 local React = require(ReplicatedStorage.Packages.React)
+
+--[ React Components ]--
+local Menu = require(script.Parent.Menu)
 local HUD = require(script.Parent.HUD)
 
 --[ Return Component ]--
 return function()
+	-- Create state for HUD visibility
+	local showHUD: boolean, setShowHUD: (newVisibility: boolean) -> () = React.useState(false)
+
+	-- Handle visibility
+	React.useEffect(function() end)
+
+	-- Return app
 	return React.createElement(React.Fragment, nil, {
-		HUD = React.createElement(HUD), -- Create HUD
+		Menu = not showHUD and React.createElement(Menu), -- Render menu if HUD is disabled
+		HUD = showHUD and React.createElement(HUD), -- Render HUD if enabled
 	})
 end
